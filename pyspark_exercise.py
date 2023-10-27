56e2e76df39679d2effdc5ac0c6d2464caacef4b
Lis-Krzysztof <krzysztof.lis@schibsted.com>
2023-10-27 09:44:13 +0200

    Add final touches

--- pyspark_exercise.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-
-
-
-parDF1=spark.read.parquet("inventory.parquet")
-
-
-print(parDF1)
